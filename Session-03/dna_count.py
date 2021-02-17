d880146937506192ea85a279613cbc9022a5f0b8
mburgosc <m.burgos.2020@alumnos.urjc.es>
2021-02-17 18:38:39 +0100

    3 ex 2

---
+++ Session-03/dna_count.py
@@ -0,0 +1,34 @@
+def correct_seq(dna):
+    for ch in dna:
+        if ch != "A" and ch != "C" and ch != "G" and ch != "T":
+            return False
+
+    return True
+
+
+
+
+
+def count(dna):
+    a, c, g, t = 0, 0, 0, 0
+    for i in dna:
+        if i == "A":
+            a += 1
+        elif i == "C":
+            c += 1
+        elif i == "G":
+            g += 1
+        elif i == "T":
+            t += 1
+    return a, c, g, t
+
+dna = input("Enter the sequence: ")
+if correct_seq(dna):
+    print("Total length: ", len(dna))
+    a, c, g, t = count(dna)
+    print("A", a)
+    print("C", c)
+    print("G", g)
+    print("T", t)
+else:
+    print("Not a valid sequence")
