0128dfd6432fc79dedf46cf97dedb810a84e37e7
mburgosc <m.burgos.2020@alumnos.urjc.es>
2021-03-09 16:46:33 +0100

    exercises P1

---
+++ P1/EX2-EX3.py
@@ -0,0 +1,10 @@
+from P1.Seq1 import Seq
+
+print("-----|Practice 1, Exercise 2,3|-----")
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("jskdhs")
+
+print("Sequence 1: " + str(s1))
+print("Sequence 2: " + str(s2))
+print("Sequence 3: " + str(s3))
---
+++ P1/EX5.py
@@ -0,0 +1,10 @@
+from P1.Seq1 import Seq
+
+print("-----|Practice 1, Exercise 5|-----")
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("jskdhs")
+
+print("Sequence 1: " +  ": (Length:" + str(s1.len()) + " ) " + str(s1))
+print("Sequence 2: " +  ": (Length:" + str(s2.len()) + " ) " + str(s2))
+print("Sequence 3: " +  ": (Length:" + str(s3.len()) + " ) " + str(s3))
