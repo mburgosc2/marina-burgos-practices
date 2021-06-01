8208fb4f3db67e8944635ac3f3fede9944e3db00
mburgosc <m.burgos.2020@alumnos.urjc.es>
2021-06-01 22:43:15 +0200

    FINAL PROJECT

---
+++ FINAL-PROJECT/server.py
@@ -0,0 +1,114 @@
+
+import http.client
+import http.server
+import socketserver
+import termcolor
+from jinja2 import Template
+from pathlib import Path
+import json
+from urllib.parse import urlparse, parse_qs
+import server_utils
+
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+        o = urlparse(self.path)
+        path_name = o.path
+        arguments = parse_qs(o.query)
+        print("RESOURCE REQUESTED: ", path_name)
+        print("PARAMETERS: ", arguments)
+        context = {}
+        if path_name == "/":
+            contents = server_utils.read_template_html_file("./html/index.html").render(context=context)
+        elif path_name == "/listSpecies":
+            dict_s = server_utils.get_data("/info/species")
+            try:
+                limit = arguments["limit"][0]
+            except KeyError:
+                limit = len(dict_s["species"])
+            contents = server_utils.list_species(dict_s, limit)
+        elif path_name == "/karyotype":
+            try:
+                specie_name = arguments["specie"][0]
+                dict_k = server_utils.get_data("info/assembly" + "/" + specie_name)
+                contents = server_utils.karyotype(dict_k, specie_name)
+            except KeyError:
+                contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+
+        elif path_name == "/chromosomeLength":
+            if len(arguments) == 2:
+                try:
+                    specie= arguments["specie"][0]
+                    chromo = arguments["chromo"][0]
+                    dict_c = server_utils.get_data("/info/assembly" + "/" + specie + "/" + chromo )
+                    contents = server_utils.length_chromosome(dict_c, specie, chromo)
+                except KeyError:
+                    contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+            else:
+                contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+
+        elif path_name == "/geneSeq":
+            try:
+                gene = arguments["gene"][0]
+                dict_seq = server_utils.get_data("/sequence/id/" + server_utils.genes_dict[gene])
+                contents = server_utils.sequence(dict_seq, gene)
+            except KeyError:
+                contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+        elif path_name == "/geneInfo":
+            try:
+                gene = arguments["gene"][0]
+                dict_info = server_utils.get_data("/sequence/id/" + server_utils.genes_dict[gene])
+                contents = server_utils.infoseq(dict_info, gene)
+            except KeyError:
+                contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+        elif path_name == "/geneCalc":
+            try:
+                gene = arguments["gene"][0]
+                dict_calc = server_utils.get_data("/sequence/id/" + server_utils.genes_dict[gene])
+                contents = server_utils.calcseq(dict_calc, gene)
+            except KeyError:
+                contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+
+        else:
+            contents = server_utils.read_template_html_file("./html/ERROR.html").render()
+
+        self.send_response(200)
+        length = len(contents.encode())
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', str(length))
+        # The header is finished
+        self.end_headers()
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+        return
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
---
+++ FINAL-PROJECT/server_utils.py
@@ -0,0 +1,134 @@
+
+from Seq2 import Seq
+from pathlib import Path
+from jinja2 import Template
+import http.server
+import http.client
+import json
+
+genes_dict = {
+    "FRAT1": "ENSG00000165879",
+    "ADA":  "ENSG00000196839",
+    "FXN":  "ENSG00000165060",
+    "RNU6_269P": "ENSG00000212379",
+    "MIR633": "ENSG00000207552",
+    "TTTY4C": "ENSG00000228296",
+    "RBMY2YP": "ENSG00000227633",
+    "FGFR3": "ENSG00000068078",
+    "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
+
+
+def read_template_html_file(filename):
+    contents = Template(Path(filename).read_text())
+    return contents
+
+def print_color(message, color):
+    import termcolor
+    print(termcolor.colored(message, color))
+
+
+
+def get_data(ENDPOINT):
+    SERVER = "rest.ensembl.org"
+    PARAMS = "?content-type=application/json"
+    print(SERVER + ENDPOINT + PARAMS)
+    connection = http.client.HTTPConnection(SERVER)
+    connection.request("GET", ENDPOINT + PARAMS)
+    response = connection.getresponse()
+    print("Response received!:", response.status, response.reason)
+    answer_decoded = response.read().decode("utf-8")
+    dict_response = json.loads(answer_decoded)
+    print(dict_response)
+    return dict_response
+
+def list_species(dict_response, limit):
+    try:
+        number_s = len(dict_response["species"])
+        list_s = []
+        for n in range(0, int(limit)):
+
+            try:
+                list_s.append(dict_response["species"][n]["display_name"])
+                context = {"limit": limit, "list_species": list_s, "number_s": number_s}
+                contents = read_template_html_file("./html/list_species.html").render(context=context)
+            except KeyError:
+                pass
+            except IndexError:
+                pass
+
+    except ValueError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+def karyotype(dict_response, specie_name):
+    try:
+        context = {"specie_name": specie_name, "karyotype": dict_response["karyotype"]}
+        contents = read_template_html_file("./html/karyotype.html").render(context=context)
+    except KeyError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+def length_chromosome(dict_response, specie_name, chromo):
+    try:
+        length = dict_response["length"]
+        context = {"specie": specie_name, "chromosome": chromo, "length": length}
+        contents = read_template_html_file("./html/chromosome_length.html").render(context=context)
+    except KeyError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+def sequence(dict_response,gene):
+    try:
+        seq = dict_response["seq"]
+        context = {"gene": gene, "seq": seq}
+        contents = read_template_html_file("./html/geneSeq.html").render(context=context)
+    except KeyError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+def infoseq(dict_response, gene):
+    try:
+        desc = dict_response["desc"].split(":")
+        desc_name = desc[1]
+        desc_start = desc[3]
+        desc_end = desc[4]
+        id = dict_response["id"]
+        len =  int(desc_end) - int(desc_start) + 1
+        context = {"len": len,
+                   "id": id,
+                   "gene_name": gene,
+                   "start": desc_start,
+                   "end": desc_end,
+                   "chrom_name": desc_name}
+        contents = read_template_html_file("./html/geneInfo.html").render(context=context)
+    except KeyError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+def calcseq(dict_response, gene):
+    try:
+        seq = Seq(dict_response["seq"])
+        count_bases = seq.count()
+        percentage_bases = seq.percentage_base(seq.count_bases(), seq.len())
+        context = {"length_bases": count_bases,
+                   "percentage_bases": percentage_bases,
+                   "gene_name": gene,
+                   "total_length": seq.len()}
+        contents = read_template_html_file("./html/geneCalc.html").render(context=context)
+    except KeyError:
+        contents = read_template_html_file("./html/ERROR.html").render()
+    return contents
+
+
+
+
+
+
+
+
+
+
+
+
